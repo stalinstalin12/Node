a43b66e03928bb6ed5d9b395355f7f8a668c9798
stalin <mstalin129@gmail.com>
2024-08-22 10:59:33 +0530

    hi

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const url=require('url')
 const http=require('http');
 const fs=require('fs');
+const querystring=require('querystring');
 const port=3000;
 
 const server= http.createServer((req,res)=>{
@@ -18,6 +19,24 @@ const server= http.createServer((req,res)=>{
     else if(parsed_url.pathname==='/style.css'){
         res.writeHead(200,{'Content-Type':'text/css'});
         res.end(fs.readFileSync('../client/style.css'));
+    }
+    else if(parsed_url.pathname==='/submit'&& req.method==='POST'){
+        console.log("reached");
+        let body='';
+        req.on('data',(chunks)=>{
+            console.log(chunks);
+            body+=chunks.toString();
+        });
+        req.on('end',()=>{
+            console.log(body);
+
+            let datas=querystring.parse(body);
+            console.log(datas);
+            console.log(datas.name);
+            console.log(datas.email);
+            console.log(datas.pass);
+
+        })
     }
 
 });
