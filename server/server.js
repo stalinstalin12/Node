0d06cfcab85b7333037d076e8a57d4a6b03032d0
stalin <mstalin129@gmail.com>
2024-08-29 10:53:07 +0530

    hi

--- server/server.js
+++ server/server.js
@@ -4,7 +4,24 @@ const fs=require('fs');
 const querystring=require('querystring');
 const port=3000;
 
+const {MongoClient}=require('mongodb');
+
+const client=new MongoClient('mongodb://localhost:27017');
+async function connect(){
+    try{
+        await client.connect();
+        console.log("connection established");
+
+    }
+    catch(error){
+        console.log("error",error);
+    }
+}
+connect();
+
 const server= http.createServer((req,res)=>{
+    let db=client.db("dms");
+    let collection=db.collection("users");
 
     const req_url=req.url;
     console.log(req_url);
@@ -20,6 +37,7 @@ const server= http.createServer((req,res)=>{
         res.writeHead(200,{'Content-Type':'text/css'});
         res.end(fs.readFileSync('../client/style.css'));
     }
+   
     else if(parsed_url.pathname==='/submit'&& req.method==='POST'){
         console.log("reached");
         let body='';
@@ -36,6 +54,25 @@ const server= http.createServer((req,res)=>{
             console.log(datas.email);
             console.log(datas.pass);
 
+            //save to db
+
+            collection.insertOne({
+                name:datas.name,
+                email:datas.email,
+                pass:datas.pass,
+            })
+            .then((message)=>{
+                console.log("message",message);
+                res.writeHead(201,{"Content-Type":"text/plain"});
+                res.end("user created successfully");
+            })
+            .catch((error)=>{
+                console.log("error", error);
+
+                res.writeHead(400,{"Content-Type":"text/plain"});
+                res.end(error.message?error.message:"user created failed");
+            })
+
         })
     }
 
@@ -44,3 +81,5 @@ server.listen(port,()=>{
    console.log(`server running at http://localhost:${port}`) ;
 
 });
+
+
